dbd3ac2277d61dc2e18b21f56107df2d13fa53ff
Sam Nelson <sam.nelson@ti.com>
2015-09-14 15:58:41 -0400

    uio-ti-pdrv: Add uio based driver for TI peripheral modules
    - Provide generic uio driver for TI peripheral modules
    - Provides access to peripheral registers and memory sections
    - Also ties-in interrupt and clock operations.
    
    Signed-off-by: Sam Nelson <sam.nelson@ti.com>

---
+++ include/uapi/linux/uio_ti_pdrv.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2015 Texas Instruments Incorporated
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _UAPI__UIO_TI_PDRV_H
+#define _UAPI__UIO_TI_PDRV_H
+
+#define UIO_TI_PDRV_MAP_INDEX_MASK 0x7
+#define UIO_TI_PDRV_MAP_OFFSET_SHIFT 3
+
+#endif /* _UAPI__UIO_TI_PDRV_H */
