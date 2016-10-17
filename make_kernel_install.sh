61affdb73f987eab41fd5ae488b26cfe5b82b4a0
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2016-10-17 15:37:59 +0200

    Making the same naming convetions as other projects

--- make_kernel_install.sh
+++ make_kernel_install.sh
@@ -5,8 +5,8 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage LOADADDR=0x80200000
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- var-som-am33-urtu.dts
 
-cp ${URTU_KERNEL_BOOT}/zImage ${URTU_KERNEL_BIN}/zImage-urtu
-cp ${URTU_KERNEL_BOOT}/dts/var-som-am33-urtu.dtb ${URTU_KERNEL_BIN}/deviceTree-urtu.dtb
+cp ${URTU_KERNEL_BOOT}/zImage ${URTU_KERNEL_BIN}/zImage-4.1.6-urtu
+cp ${URTU_KERNEL_BOOT}/dts/var-som-am33-urtu.dtb ${URTU_KERNEL_BIN}/devicetree-zImage-var-som-am33-urtu.dtb
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules_install INSTALL_MOD_PATH=${URTU_KERNEL_MODULES}
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- headers_install INSTALL_HDR_PATH=${URTU_KERNEL_INCLUDE}
