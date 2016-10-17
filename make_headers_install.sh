0ae41f5e0a77e256a46ea7fc111a025634675fb1
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2016-10-17 15:08:46 +0200

    Asking for confirmation

--- make_headers_install.sh
+++ make_headers_install.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- headers_install INSTALL_HDR_PATH=~/kernel-out-of-tree/VAR-SOM-AMx3-Kernel-4-1-include
+echo "Output directory: ${URTU_KERNEL_MODULES}"
+
+read  -n 1 -p "Continue ? [y/n]" mainmenuinput
+
+echo
+
+if [ "$mainmenuinput" = "y" ] ; then
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- headers_install INSTALL_HDR_PATH=~/kernel-out-of-tree/VAR-SOM-AMx3-Kernel-4-1-include
+fi
 
--- make_modules_install.sh
+++ make_modules_install.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules_install INSTALL_MOD_PATH=~/kernel-out-of-tree/VAR-SOM-AMx3-Kernel-4-1-modules
+echo "Output directory: ${URTU_KERNEL_MODULES}"
+
+read  -n 1 -p "Continue ? [y/n]" mainmenuinput
+
+echo
+
+if [ "$mainmenuinput" = "y" ] ; then
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules_install INSTALL_MOD_PATH=~/kernel-out-of-tree/VAR-SOM-AMx3-Kernel-4-1-modules
+fi
+
