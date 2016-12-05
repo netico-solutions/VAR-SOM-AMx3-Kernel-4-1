cba23d87ab4cd0244fa07659d3b419152d53adda
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2016-12-05 14:34:48 +0100

    Added conditional installation of destination directories

--- make_kernel_install.sh
+++ make_kernel_install.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+if [ ! -d ${URTU_KERNEL_BIN} ] ; then
+    echo 'Creating: ' ${URTU_KERNEL_BIN}
+    mkdir -p ${URTU_KERNEL_BIN}
+fi
+
+if [ ! -d ${URTU_KERNEL_BOOT} ] ; then
+    echo 'Creating: ' ${URTU_KERNEL_BOOT}
+    mkdir -p ${URTU_KERNEL_BOOT}
+fi
+
 cd ${URTU_KERNEL}
+
+
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage LOADADDR=0x80200000
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- var-som-am33-urtu.dts
